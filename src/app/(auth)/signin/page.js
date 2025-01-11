b6b1dab48bbd8cf1fd367fb4433191c0505b6d48
HoTaN <id4acc2014@protonmail.com>
2025-01-11 22:24:37 +0330

    feat: :sparkles: I secured the signin and signup pages with server-side authentication.
    I used `getServerSession` from `next-auth` to prevent authenticated users from accessing the signin and signup pages.
    Added server-side redirection to the homepage if a session exists in `src/app/(auth)/signin/page.js` and `src/app/(auth)/signup/page.js`.

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -1,6 +1,11 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import SigninPage from "@/template/SigninPage";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-function Signin() {
+async function Signin() {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/");
   return <SigninPage />;
 }
 
--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -1,6 +1,12 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import SignupPage from "@/template/SignupPage";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-function Signup() {
+async function Signup() {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/");
+
   return <SignupPage />;
 }
 
