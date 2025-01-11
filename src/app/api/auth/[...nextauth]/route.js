b0c838ed4e65b3cb0b192fbf85477f3f4195d29e
HoTaN <id4acc2014@protonmail.com>
2025-01-11 13:55:09 +0330

    feat: :sparkles: I integrated NextAuth for user authentication.
    I implemented NextAuth in src/app/api/auth/[...nextauth]/route.js with a credentials provider for custom email and password authentication.
    The configuration uses JWT-based sessions and connects to the database to verify user credentials.
    This addition provides secure user login and session management.

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,36 @@
+import User from "@/models/User";
+import { verifyPassword } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions = {
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const [email, password] = credentials;
+
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error("مشکلی در سرور پیش آمده است");
+        }
+
+        if ((!email, !password)) throw new Error("لطفا تمامی فیلد ها را پر کنید");
+
+        const user = await User.findOne({ email });
+        if (!user) throw new Error("این حساب کاربری وجود ندارد");
+
+        const isValid = await verifyPassword(password, user.password);
+        if (!isValid) throw new Error(" ایمیل یا رمز عبور اشتباه است");
+
+        return { email };
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
