24ba2d9070ae83858f97bad319ae56a845a7f133
HoTaN <id4acc2014@protonmail.com>
2025-01-11 19:52:12 +0330

    feat: :sparkles: I implemented the login form with client-side validation.
    I added the `SigninPage` component with a form for user login.
    It includes input fields for email and password, a loading spinner, and toast notifications for success and error messages.
    I used the `next-auth` `signIn` method to handle login without redirection.
    A link to the signup page was also added for users without an account.

---
+++ src/components/template/SigninPage.js
@@ -0,0 +1,65 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { toast, Toaster } from "react-hot-toast";
+import { ThreeDots } from "react-loader-spinner";
+import styles from "@/template/SignupPage.module.css";
+
+function SigninPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const signinHandler = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    const res = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+    setLoading(false);
+
+    if (res.error) {
+      toast.error(res.error);
+    } else {
+      toast.success("ورود با موفقیت انجام شد");
+      setTimeout(() => {
+        router.push("/");
+      }, 1000);
+    }
+  };
+
+  return (
+    <div className={styles.form}>
+      <h4>فرم ورود</h4>
+      <form>
+        <label htmlFor="">ایمیل:</label>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <label htmlFor="">رمز عبور:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+        {loading ? (
+          <ThreeDots color="#304ffe" height={45} ariaLabel="three-dots-loading" visible={true} wrapperStyle={{ margin: "auto" }} />
+        ) : (
+          <button type="submit" onClick={signinHandler}>
+            ورود
+          </button>
+        )}
+      </form>
+      <p>
+        حساب کاربری ندارید؟
+        <Link href="/signup">ثبت نام</Link>
+      </p>
+      <Toaster />
+    </div>
+  );
+}
+
+export default SigninPage;
