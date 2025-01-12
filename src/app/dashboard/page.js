26c5bdba33199ea1eb07cf2eef9b1da6ae10b4fa
HoTaN <id4acc2014@protonmail.com>
2025-01-12 20:54:58 +0330

    feat: :sparkles: I fetched and displayed the user registration date on the dashboard.
    I updated `src/app/dashboard/page.js` to fetch the user registration date from the database using `User.findOne` and passed it as a prop to `DashboardPage`.
    In src/components/template/DashboardPage.js`, I formatted and displayed the date using `toLocaleDateString("fa-IR")` for Persian localization.
    This enhances the user experience by showing personalized account information.

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,14 @@
+import User from "@/models/User";
 import DashboardPage from "@/template/DashboardPage";
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
-function Dashboard() {
-  return <DashboardPage />;
+async function Dashboard() {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+  const user = await User.findOne({ email: session.user.email });
+  return <DashboardPage createdAt={user.createdAt} />;
 }
 
 export default Dashboard;
--- src/components/template/DashboardPage.js
+++ src/components/template/DashboardPage.js
@@ -1,12 +1,16 @@
 import styles from "@/template/DashboardPage.module.css";
 
-function DashboardPage() {
+function DashboardPage({ createdAt }) {
   return (
     <div className={styles.cotainer}>
       <h3>سلام 👋</h3>
       <p>آگهی های خود را ثبت کنید تا هزاران نفر آن را مشاهده کنند</p>
-      <div className={styles.createdAt}><p>تاریخ عضویت:
-        <span></span></p></div>
+      <div className={styles.createdAt}>
+        <p>
+          تاریخ عضویت:
+          <span>{new Date(createdAt).toLocaleDateString("fa-IR")}</span>
+        </p>
+      </div>
     </div>
   );
 }
