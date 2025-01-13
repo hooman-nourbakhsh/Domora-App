999ad449b5ee0cbe6974d27f5d31627841b47d0d
HoTaN <id4acc2014@protonmail.com>
2025-01-13 18:55:37 +0330

    feat: :sparkles: I added a page to fetch and display user-specific profiles
    Implemented a MongoDB aggregation query in `src/app/dashboard/my-profiles.js/page.js` to retrieve profiles associated with the logged-in user.
    This integration allows fetching user-specific listings directly from the database for rendering in the dashboard.

---
+++ src/app/dashboard/my-profiles.js/page.js
@@ -0,0 +1,25 @@
+import { getServerSession } from "next-auth";
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+
+async function MyProfiles() {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+
+  const [user] = await User.aggregate([
+    {
+      $match: { email: session.user.email },
+    },
+    {
+      $lookup: {
+        from: "profiles",
+        foreignField: "userId",
+        localField: "_id",
+        as: "profiles",
+      },
+    },
+  ]);
+  return <div>MyProfiles</div>;
+}
+
+export default MyProfiles;
