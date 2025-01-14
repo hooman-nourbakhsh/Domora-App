28de9accdec696c979f741e4a42dea5bf036bda3
HoTaN <id4acc2014@protonmail.com>
2025-01-14 17:31:58 +0330

    feat: :sparkles: Implemented profile update API endpoint.
    Enhanced `src/app/api/profile/route.js` by adding a `PATCH` method to handle profile updates.
    Validated user session, checked user authorization, and updated the specified profile.
    Improved error handling for missing data and unauthorized access.

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -9,13 +9,13 @@ export async function POST(req) {
   try {
     await connectDB();
 
-    const body = await req.json();
-    const { title, description, location, phone, price, realEState, constructionDate, category, rules, amenities } = body;
+    const { title, description, location, phone, price, realEState, constructionDate, category, rules, amenities } = await req.json();
+
     const session = await getServerSession(req);
     if (!session) {
       return NextResponse.json({ error: "لطفا وارد حساب کاربری خود شوید" }, { status: 401 });
     }
-    
+
     const user = await User.findOne({ email: session.user.email });
     if (!user) {
       return NextResponse.json({ error: "این حساب کاربری وجود ندارد" }, { status: 404 });
@@ -24,6 +24,7 @@ export async function POST(req) {
     if (!title || !description || !location || !phone || !price || !realEState || !constructionDate || !category) {
       return NextResponse.json({ error: "لطفا تمامی فیلد ها را پر کنید" }, { status: 422 });
     }
+
     const newProfile = await Profile.create({
       title,
       description,
@@ -38,6 +39,50 @@ export async function POST(req) {
       userId: new Types.ObjectId(user._id),
     });
     return NextResponse.json({ message: "اگهی با موفقیت ایجاد شد" }, { status: 201 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "مشکلی در سرور پیش آمده است" }, { status: 500 });
+  }
+}
+
+export async function PATCH(req) {
+  try {
+    await connectDB();
+
+    const { _id, title, description, location, phone, price, realEState, constructionDate, category, rules, amenities } = await req.json();
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json({ error: "لطفا وارد حساب کاربری خود شوید" }, { status: 401 });
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json({ error: "این حساب کاربری وجود ندارد" }, { status: 404 });
+    }
+
+    if (!_id || !title || !description || !location || !phone || !price || !realEState || !constructionDate || !category) {
+      return NextResponse.json({ error: "لطفا تمامی فیلد ها را پر کنید" }, { status: 422 });
+    }
+
+    const profile = await Profile.findOne({ _id });
+    if (!user._id.equals(profile.userId)) {
+      return NextResponse.json({ error: "دسترسی شما به این آگهی محدود شده است" }, { status: 403 });
+    }
+
+    profile.title = title;
+    profile.description = description;
+    profile.location = location;
+    profile.phone = phone;
+    profile.realEState = realEState;
+    profile.price = +price;
+    profile.constructionDate = constructionDate;
+    profile.amenities = amenities;
+    profile.rules = rules;
+    profile.category = category;
+    profile.save();
+
+    return NextResponse.json({ message: "اگهی با موفقیت ویرایش شد" }, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json({ error: "مشکلی در سرور پیش آمده است" }, { status: 500 });
