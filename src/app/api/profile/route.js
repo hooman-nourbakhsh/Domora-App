c7ccc2ff15723ec7725e650312ad2b02b226278a
HoTaN <id4acc2014@protonmail.com>
2025-01-15 18:32:04 +0330

    feat: :sparkles: I developed an API endpoint to retrieve a list of profiles.
    I developed a GET API endpoint in src/app/api/profile/route.js to fetch all profile listings from the database.
    The implementation queries the Profile collection, excluding the userId field for privacy reasons, and returns the data in JSON format with appropriate status codes.
    Error handling is also implemented to manage server issues gracefully.
    This API improves data access for frontend components that need to display profile information.

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -5,6 +5,18 @@ import { Types } from "mongoose";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
+export async function GET(req) {
+  try {
+    await connectDB();
+    const profiles = await Profile.find().select("-userId");
+
+    return NextResponse.json({ data: profiles }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "مشکلی در سرور پیش آمده است" }, { status: 500 });
+  }
+}
+
 export async function POST(req) {
   try {
     await connectDB();
