3f88065d6a68ec249e24ca61678b88c09b568fb2
HoTaN <id4acc2014@protonmail.com>
2025-01-17 21:50:23 +0330

    feat: :sparkles: I restricted access to the admin panel based on user roles.
    In src/app/admin/page.js, I implemented access control by verifying the user's role after fetching session details and user data from the database.
    If a user is not authenticated, they are redirected to the sign-in page, and users without the ADMIN role are redirected to their dashboard.
    This ensures only users with administrative privileges can access the admin page, enhancing security and proper role-based control in the application.

---
+++ src/app/admin/page.js
@@ -0,0 +1,21 @@
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import User from "@/models/User";
+import DashboardSidebar from "@/layout/DashboardSidebar";
+
+async function Admin() {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/signin");
+
+  const user = await User.findOne({ email: session.user.email });
+  if (user.role !== "ADMIN") redirect("/dashboard");
+  return (
+    <DashboardSidebar role={user.role} email={user.email}>
+      admin
+    </DashboardSidebar>
+  );
+}
+
+export default Admin;
